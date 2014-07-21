fc3d7cf7ebe3c45d7391291c16ea54a2cb173e11
Simon Gaestel <simon.gaestel+git@gmail.com>
2014-07-21 21:41:51 +0200

    Fix tests

--- test/test_cucumber_completion.py
+++ test/test_cucumber_completion.py
@@ -25,18 +25,18 @@ def check_examples(d):
 
 
 def test_splitting_regex_by_no_groups():
-    assert list(completer.create_completion_text("no groups", [])) == ["no groups"]
+    assert list(completer.create_completion_text("no groups", '')) == ["no groups"]
 
 
 def test_splitting_regex_by_groups():
-    chunks = list(completer.create_completion_text('The customers name is (.*)', ['group1']))
+    chunks = list(completer.create_completion_text('The customers name is (.*)', 'group1'))
     assert 'The customers name is (group1)'
-    chunks = list(completer.create_completion_text('Mrs (.*) is a customer', ['group1']))
+    chunks = list(completer.create_completion_text('Mrs (.*) is a customer', 'group1'))
     assert 'Mrs (group1) is a customer' == chunks
-    chunks = list(completer.create_completion_text('(Mr|Mrs|Ms) (.*) is a customer', ['group1', 'group2']))
+    chunks = list(completer.create_completion_text('(Mr|Mrs|Ms) (.*) is a customer', 'group1, group2'))
     assert '(group1) (group2) is a customer' == chunks
 
 
 def test_splitting_regex_ignores_outer_braces():
-    chunks = list(completer.create_completion_text('More (braces (arent)) groups', ['group1']))
+    chunks = list(completer.create_completion_text('More (braces (arent)) groups', 'group1'))
     assert ['More (braces (group1)) groups'] == chunks
